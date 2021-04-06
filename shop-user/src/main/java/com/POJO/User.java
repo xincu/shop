ec6076b9867b1c682d8005e0fc9f743ee711c816
zhoutong@ysstech.com <zhoutong@ysstech.com>
2021-04-06 16:35:17 +0800

    关于分布式架构

---
+++ shop-user/src/main/java/com/POJO/User.java
@@ -0,0 +1,16 @@
+package com.POJO;
+
+import com.common.POJO.people;
+import lombok.Data;
+
+/**
+ * @ClassName: User
+ * @Description:
+ * @Date: 2021/4/6 14:54
+ * @Author: zt
+ * @Version: 1.0
+ */
+@Data
+public class User  extends people {
+    private String name;
+}
